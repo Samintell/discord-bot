d2b8a7c7937c8125cadc0c9878f638a64175d07e
Samintell <joshuackarim@gmail.com>
2026-01-29 22:21:54 -0600

    update translation

--- convert_data.py
+++ convert_data.py
@@ -171,7 +171,8 @@ def get_english_translation(title, artist, song_id):
         '泥の分際で私だけの大切を奪おうだなんて': 'Even Though I\'m Just Mud, Don\'t Take Away What\'s Important to Me',
         'チルノのパーフェクトさんすう教室　⑨周年バージョン': 'Cirno\'s Perfect Math Class 9th Anniversary Version',
         'スカーレット警察のゲットーパトロール24時': 'Scarlet Police Ghetto Patrol 24 Hours',
-        'バカ通信': 'Idiotic Transaction'
+        'バカ通信': 'Idiotic Transaction',
+        'み　む　かｩ　わ　ナ　イ　ス　ト　ラ　イ　ク': 'Mimukauwa Nice Try'
     }
     
     if title in known_translations:
@@ -210,6 +211,7 @@ def get_romaji_override(title):
         'False Amber (from the Black Bazaar, Or by A Kervan Trader from the Lands Afar, Or Buried Beneath the Shifting Sands That Lead Everywhere but Nowhere)': 'False Amber',
         'パラマウント☆ショータイム！！': 'Paramount Showtime!!',
         'VeRForTe αRtE:VEiN': 'Verforte Arte Vein'
+        ''
     }
     return romaji_overrides.get(title, None)
 
